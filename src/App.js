555ef5f86270001588e314e02714f94e3ad4637a
bukirwa99 <angellabukirwa99@gmail.com>
2020-02-12 17:41:06 +0300

    create single movie page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {BrowserRouter as Router ,Route,Switch,link}
 from 'react-router-dom'
 import './App.css';
-import {BrowserRouter as Router, Route  } from "react-router-dom";
 import Home from './home'
+import Movie from './movie'
 function App() {
   return (<Router>
     <div>
-      <Route exact path="/"component={Home}/>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/movie" component={Movie}/>
     </div>
 e
     </Router>);
